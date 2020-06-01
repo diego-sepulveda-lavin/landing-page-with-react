cc1aae08a74ba8eb447b54b60654e4d2c7ef8fd6
Diego <diegosl@hotmail.cl>
2020-06-01 16:51:47 -0400

    Primer Componente de prueba

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import React from "react";
+import ReactDOM from 'react-dom';
+
+import Navbar from './componentes/navbar/navbar'
+
+
+
+
+
+
+ReactDOM.render(<Navbar />, document.getElementById('root'))
+
